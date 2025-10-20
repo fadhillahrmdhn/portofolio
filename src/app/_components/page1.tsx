ff7da3cc70ff93edba92061f9712ac7c28c95fad
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-10-20 14:00:18 +0700

    feat: add GradientText component with customizable gradient animation and integrate into Page1

---
+++ src/app/_components/page1.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import GradientText from '@/components/GradientText';
+
+export const Page1 = () => {
+  return (
+    <div className="flex flex-col gap-6 items-center">
+      <GradientText colors={['#40ffaa', '#4079ff', '#40ffaa', '#4079ff', '#40ffaa']} animationSpeed={3} showBorder={false} className="custom-class text-4xl sm:text-5xl md:text-6xl  px-6">
+        Hei, I'm a Frontend Developer
+      </GradientText>
+      <p className="text-white text-center text-lg sm:text-xl max-w-3xl">
+        Passionate about building modern, responsive, and performant web experiences.
+      </p>
+    </div>
+  );
+};
