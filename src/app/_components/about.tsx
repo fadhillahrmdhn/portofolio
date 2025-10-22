649a82aa1316a7404f0aaad143ac17767b38e3ae
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-10-22 17:24:19 +0700

    feat: update About component styling; adjust margin and layout for improved readability

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -9,7 +9,7 @@ export const About = () => {
     <section className="h-screen snap-start flex flex-col gap-4 w-full bg-gradient-to-b from-[#2D425C] via-slate-900 to-black pt-10 pb-20">
       <motion.div initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.2 }} viewport={{ once: false }} className="max-w-4xl mx-auto px-4 text-center">
         <h2 className="text-3xl sm:text-4xl md:text-5xl text-cyan-400 font-bold mb-4 font-mono tracking-wide">About Me</h2>
-        <hr className="mx-auto mb-6 h-1 w-24 rounded-full border-0 bg-gradient-to-r from-cyan-500 via-blue-500 to-purple-500" />
+        <hr className="mx-auto mb-4 h-1 w-24 rounded-full border-0 bg-gradient-to-r from-green-400 via-cyan-500 to-blue-500" />
 
         <p className="mx-auto text-center max-w-3xl text-slate-100 text-base sm:text-lg  leading-relaxed">Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</p>
       </motion.div>
@@ -18,11 +18,11 @@ export const About = () => {
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.4 }}
         viewport={{ once: false }}
-        className=" mx-auto px-4 text-center text-white text-base sm:text-lg md:text-xl leading-relaxed grid grid-cols-2 gap-28 mt-10"
+        className=" mx-auto px-4 text-center text-white text-base sm:text-lg md:text-xl leading-relaxed grid grid-cols-2 gap-28 mt-5"
       >
         <div className="max-w-md mx-auto">
           <h3 className="text-2xl font-bold mb-4 font-mono tracking-wide text-cyan-400">Get to know me!</h3>
-          <p className="mt-4 text-justify text-slate-100">
+          <p className="text-justify text-slate-100">
             I'm a passionate frontend developer with a strong focus on creating user-friendly and visually appealing web applications. I strive to build responsive and dynamic interfaces that enhance user experience. I'm always eager to
             learn new technologies and improve my skills. I also enjoy working with teams and always strive to make meaningful contributions to every project I work on.
           </p>
