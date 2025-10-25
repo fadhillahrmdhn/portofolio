14164b6a7a0adbea601290cb38eb82e86f1f70d2
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-10-25 17:11:18 +0700

    refactor: update ButtonLiveDemo component styling for consistent button width

--- src/app/_components/buttonLiveDemo.tsx
+++ src/app/_components/buttonLiveDemo.tsx
@@ -6,7 +6,7 @@ export const ButtonLiveDemo = ({ link }: { link: string }) => {
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 font-medium rounded-lg text-sm py-2.5 text-center me-2 mb-2 w-1/4"
     >
       Visit Site
     </a>
