9e3ab848c28a2128358dffb43140a454d4baf41b
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-10-25 21:14:05 +0700

    refactor: restore ButtonLiveDemo component styling for consistent gradient background

--- src/app/_components/buttonLiveDemo.tsx
+++ src/app/_components/buttonLiveDemo.tsx
@@ -6,7 +6,7 @@ export const ButtonLiveDemo = ({ link }: { link: string }) => {
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 font-medium rounded-lg text-sm py-2.5 px-5 text-center me-2 mb-2 font-mono "
+      className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300  shadow-lg shadow-cyan-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 font-mono "
     >
      Visit site
     </a>
