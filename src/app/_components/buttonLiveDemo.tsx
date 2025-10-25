537d30781b00175690a2ea77d8ab795af3845d7a
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-10-25 20:58:49 +0700

    refactor: update ButtonLiveDemo component styling for consistent text casing

--- src/app/_components/buttonLiveDemo.tsx
+++ src/app/_components/buttonLiveDemo.tsx
@@ -6,9 +6,9 @@ export const ButtonLiveDemo = ({ link }: { link: string }) => {
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 font-medium rounded-lg text-sm py-2.5 px-5 text-center me-2 mb-2"
+      className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 font-medium rounded-lg text-sm py-2.5 px-5 text-center me-2 mb-2 font-mono "
     >
-      Visit Site
+     Visit site
     </a>
   );
 };
